98bdd3fc6ec57e6500d6e8568ba16235d8261480
Gaurav Kumar <121447501+SHARMA79822@users.noreply.github.com>
2024-06-08 18:51:10 +0000

    container config done

---
+++ .github/Basic/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // Treat all JS Code as newer version
+
+//alert(3 + 3) // We are using node.js, not Browser
+
+
+// Primitive Data Types
+let name = "Gaurav"; // String Data Types
+let age = 18; // Number Data Types
+let isLoggedIn = false; // Boolean Data Types
+let num = 123456789458252; // bigInt Data Types
+let state = null; // Null Data Types
+let undefine; // Undefine Data Types
+
+
+// Non Primitive Data Types
+//  i) Object
+// ii) Array
---
+++ .github/Basic/variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553; // We can't Change the value
+let accountEmail = "kumar7998@gmail.com";// It is Used for the new browser.
+var accountPassword = "12345";// It is used to old Browser.
+accountCity = "Mayur Vihar";
+let accountState;
+
+//accountId = 2;
+console.log(accountId);
+
+accountEmail = "gaurv@gmail.com"
+
+accountPassword = "56785"
+
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountPassword])
