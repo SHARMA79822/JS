cb9a32b797094d2c9dd1959767c375c91f6d4e6c
Gaurav Kumar <121447501+SHARMA79822@users.noreply.github.com>
2024-06-11 14:18:26 +0000

    container config done

---
+++ .github/Basic/string.js
@@ -0,0 +1,18 @@
+const name = "Gaurav";
+const repocount = 20;
+
+//console.log(name + repocount)
+
+console.log(`Hello my nmae is ${name} and my repo Count is ${repocount}`);
+
+const gameName = new String('Gaurav');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('v'));
+
+
